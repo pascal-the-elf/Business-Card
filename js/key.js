2cd2287c49f680ac21afdda86318f744e673beb7
JacobLinCool <jacoblin2011@gmail.com>
2019-05-19 00:35:15 +0800

    Create key.js

---
+++ js/key.js
@@ -0,0 +1,27 @@
+var DataInURL = {};
+var u = location.href;
+if(u.search("\\?")!=-1) u = u.substr(u.search("\\?"));
+alert(u);
+var _f = 1;
+while(_f || u.search("\\&")!=-1) {
+    _f = 0;
+    var k1=1, k2, v1, v2, tmp=0;
+    while(u.charAt(tmp)!="=") {
+        if(tmp < u.length) tmp++;
+        else break;
+    }
+    k2 = tmp;
+    v1 = tmp + 1;
+    while(u.charAt(tmp)!="&") {
+        if(tmp < u.length) tmp++;
+        else break;
+    }
+    v2 = tmp;
+    if(tmp > u.length) break;
+    DataInURL[u.substring(k1, k2)] = u.substring(v1, v2);
+    u = u.substr(tmp);
+}
+
+function url(key) {
+    return DataInURL[key];
+}
